8a805a2917c032fbf257011e53733510931d393f
ynjn <60029025+ynjn@users.noreply.github.com>
2024-11-15 20:39:03 +0000

    icon launcher

--- setIcon.sh
+++ setIcon.sh
@@ -4,5 +4,5 @@ cp ic_launcher-web.png ic_launcher-web-round.png source-code/app/src/main
 
 cp -r mipmap-* source-code/app/src/main/res
 
-
+cp -r drawable/* source-code/app/src/main/res/drawable/
 
